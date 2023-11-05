0be4d4916fc17ffd1c0f500c53a86a63129dec9e
MateusHSRizzo <mateusrizzo@yahoo.com>
2023-11-05 17:47:43 -0300

    Iniciando contrução da pagina de login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using Controle_De_OS.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Controle_De_OS.Controllers
+{
+    public class LoginController : Controller
+    {
+     
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Entrar(LoginModel loginModel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if(loginModel.Login == "adm" && loginModel.Senha == "123") 
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+
+                    TempData["MensagemErro"] = $"Usuário e/ou senha inválido(s). Por favor, tente novamente.";
+
+
+
+                }
+                return View("Index");
+            }
+            catch (Exception erro) 
+            {
+                TempData["MensagemErro"] = $"Ops... Não foi possivel validar seu login, tente novamente,detalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+
+            }
+        }
+    }
+}
---
+++ Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Controle_De_OS.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Insira o Usuario")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Insira a Senha")]
+        public string Senha { get; set; }
+    }
+}
